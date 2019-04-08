e14389982fdbdc3fd99cb2d4bf106ebedbe099fb
Javier Arroyo <javier.arroyo@microhealth.com>
2019-04-08 10:25:51 +0200

    Leak memory fixed

--- app/src/main/java/es/jarroyo/books/ui/booksList/activity/BooksListActivity.kt
+++ app/src/main/java/es/jarroyo/books/ui/booksList/activity/BooksListActivity.kt
@@ -1,10 +1,15 @@
 package es.jarroyo.books.ui.booksList.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import es.jarroyo.books.R
 import es.jarroyo.books.app.di.component.ApplicationComponent
 import es.jarroyo.books.app.di.subcomponent.booksList.activity.BooksListActivityModule
 import es.jarroyo.books.ui.base.BaseActivity
+import es.jarroyo.books.ui.bookDetails.activity.BookDetailsActivity
 import es.jarroyo.books.ui.booksList.fragment.BooksListFragment
 import es.jarroyo.books.ui.booksList.fragment.adapter.ItemBook
 
@@ -25,6 +30,17 @@ class BooksListActivity : BaseActivity(), BooksListFragment.OnFragmentListener {
     }
 
     override fun onClickBook(itemBook: ItemBook) {
-        navigator.toBookDetailsActivity(itemBook.book, itemBook.itemView.findViewById(R.id.item_rv_book_iv_avatar))
+        var bundle = Bundle()
+        bundle.putSerializable(BookDetailsActivity.ARG_EXTRA_BOOK, itemBook.book)
+
+
+        val p1 = Pair<View, String>(itemBook.itemView.findViewById(R.id.item_rv_book_iv_avatar), "transitionIVDetails")
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, p1)
+
+        val intent = Intent(this, BookDetailsActivity::class.java)
+        if (bundle != null) {
+            intent.putExtras(bundle)
+        }
+        startActivity(intent, options?.toBundle())
     }
 }
